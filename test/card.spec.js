d0b6ec0df10aaa0483f9b95c28755da8ab376443
a-honey <jungahyeon0512@khu.ac.kr>
2023-12-28 16:26:48 +0900

    다시 하기 버튼 테스트 추가

--- test/card.spec.js
+++ test/card.spec.js
@@ -1,7 +1,17 @@
-describe('카드 컴포넌트 테스트', () => {
-  it('카드 컴포넌트가 렌더링된다.', () => {
-    cy.visit('http://localhost:3000');
+describe("카드 컴포넌트 테스트", () => {
+  it("카드 컴포넌트가 렌더링된다.", () => {
+    cy.visit("http://localhost:3000");
 
-    cy.get('.card').should('be.visible');
+    cy.get(".card").should("be.visible");
+  });
+
+  it("게임이 다시 렌더링된다.", () => {
+    cy.visit("http://localhost:3000");
+
+    cy.contains("button", "카드입니다").click();
+
+    cy.contains("button", "다시 하기").click();
+
+    cy.get(".card").should("be.visible");
   });
 });
